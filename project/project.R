9b686f73f4b4024fdaa1da7f7468e78bd8ba8438
Kim Nguyen <kimng@uw.edu>
2023-02-24 00:42:29 -0800

    worked on bike data set

--- project/project.R
+++ project/project.R
@@ -1,7 +1,13 @@
 library(dplyr)
 library(ggplot2)
+library(stringr)
 
 # wd() is /SOC/project
+
+# Questions that need to be answered are:
+#    What time are most bikes in use
+#    Casual users weekdays vs. weekends/pop. day of the week
+#    Does weather have an effect on users? (popular weather)
 day <- read.csv("day.csv")
      # weathersit = "weather situation"; 1 is Clear, Few clouds, Partly cloudy, Partly cloudy
      #                                   2: Mist + Cloudy, Mist + Broken clouds, Mist + Few clouds, Mist
@@ -9,7 +15,7 @@ day <- read.csv("day.csv")
      #                                   4: Heavy Rain + Ice Pallets + Thunderstorm + Mist, Snow + Fog
 
 hour <- read.csv("hour.csv")
-
+#-------------------------------------------------------------------------------
 # What times are bikes being used the most?
 popular_time <- hour %>%
   select(hr, cnt) %>%
@@ -17,19 +23,19 @@ popular_time <- hour %>%
   summarize(round(mean(cnt), 2)) %>%
   rename(average = "round(mean(cnt), 2)")
 
-# make a pie chart
-pie_time <- popular_time %>%
-  ggplot(aes(x = "",
-         y = average,
-         fill = hr)) + geom_bar(stat = "identity",
-                                width = 1) +
-  coord_polar("y", start = 0)
-
-pie_time
-
-# testing
+# make a bar graph to show most pop. hour
 column_time <- popular_time %>%
   ggplot(aes(x = hr,
-             y = average)) + geom_col()
+             y = average)) + geom_col() + ggtitle("Average # of Users vs. Hour of Day") + ylab("Average # of Users") + xlab("Hour of Day")
 
 column_time
+#-------------------------------------------------------------------------------
+# Casual users weekdays vs. weekends
+# $weekday: 0 = Sunday, 6 = Saturday
+cas_day <- day %>%
+  select(dteday, holiday, weekday, casual) %>%
+  mutate(weekday = str_replace_all(weekday, "[12345]", "weekday")) %>%
+  mutate(weekday = str_replace_all(weekday, "[06]", "weekend")) %>%
+  mutate(weekday = ifelse(holiday == 1, "holiday", weekday))
+
+# make a pie chart
